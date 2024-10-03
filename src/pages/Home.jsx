8cfd32c665b3774327a5be2cc91ac1cef380a135
Nada Elmahfoudi <elmahfoudinada17@gmail.com>
2024-10-03 15:09:37 +0100

    NADAF-2 Add Home Page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,18 @@
+
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+import Container from '../components/Container';
+
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+      <Navbar />
+      <Container />
+      <Footer />
+    </div>
+  )
+}
+
+export default Home
